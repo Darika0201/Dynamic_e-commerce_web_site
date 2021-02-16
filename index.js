afa37de13588005ae1796c38668408d2ccb1a0d8
Darika0201 <cheadarika15@gmail.com>
2021-02-16 18:35:38 +0700

    Dynamic_e-commerce_website

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookie = require('cookie-parser');
+const routes = require('./routes/routes');
+const path = require('path');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+
+const app= express();
+
+app.set('view engine','ejs');
+app.set('views', 'views');
+// let ejsOptions = {
+//     // delimiter: '?', Adding this to tell you do NOT use this like I've seen in other docs, does not work for Express 4
+//     async: true
+//   };
+app.use(bodyParser.urlencoded({entended:true}));
+
+
+
+app.use(express.static(path.join(__dirname,'publics')));
+app.use(fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+  }));
+app.use(bodyParser.json());
+app.use(cookie());
+app.use(routes);
+
+mongoose.connect('mongodb+srv://user123:user123@cluster0.nlyev.mongodb.net/user?retryWrites=true&w=majority').then(
+    result=>{
+        console.log("DB is connected")
+    }
+).catch(err=>{
+    console.log(err)
+});
+app.listen(3000);
